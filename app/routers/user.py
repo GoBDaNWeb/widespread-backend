2021c3c4c7978ea487dce792453de88380d81379
Bogdan <bo@paradigma-digital.ru>
2026-04-29 21:30:04 +0300

    fix(auth): fix jwt acces token logic

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,11 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Cookie, Depends
 
-from app.core.security import oauth2_scheme
 from app.dependencies.services import get_user_service
 
 user_router = APIRouter(prefix="/user", tags=["User"])
 
 
 @user_router.get("/me")
-async def me(token: str = Depends(oauth2_scheme), service=Depends(get_user_service)):
-    return await service.get_current_user(token)
+async def me(access_token: str = Cookie(default=None), service=Depends(get_user_service)):
+    return await service.get_current_user(access_token)
